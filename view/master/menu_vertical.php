466aa250364bc4d0ae1a44db95ecf3ce1d3753e0
heinricHz <h.sanchez@pagadito.com>
2023-10-17 01:57:19 -0600

    cambios en header

--- view/master/menu_vertical.php
+++ view/master/menu_vertical.php
@@ -8,18 +8,19 @@
         if (isset($_SESSION) && ($_SESSION['id_rol'] == '1' || $_SESSION['id_rol'] == '2' || $_SESSION['id_rol'] == '3')) {
             echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../planilla/index.php"><i class="fa-solid fa-table"></i>&nbsp;Planillas</a>';
         }
+        /*
         if (isset($_SESSION) && ($_SESSION['id_rol'] == '1' || $_SESSION['id_rol'] == '2')) {
 		    echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../costeo/index.php">Costeo de planilla</a>';
+        }*/
+        if (isset($_SESSION) && ($_SESSION['id_rol'] == '1' || $_SESSION['id_rol'] == '3' || $_SESSION['id_rol'] == '4')) {
+            echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../departamentos/index.php"><i class="fa-solid fa-people-group"></i>&nbsp;Departamentos</a>';
         }
         if (isset($_SESSION) && ($_SESSION['id_rol'] == '1' || $_SESSION['id_rol'] == '3' || $_SESSION['id_rol'] == '4')) {
-            echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../departamentos/index.php">Departamentos</a>';
-        }
-        if (isset($_SESSION) && ($_SESSION['id_rol'] == '1' || $_SESSION['id_rol'] == '3' || $_SESSION['id_rol'] == '4')) {
-            echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../empleados/index.php"><i class="fa-solid fa-user"></i>&nbsp;Empleados</a>';
+            echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../empleados/index.php"><i class="fa-solid fa-user-tie"></i>&nbsp;Empleados</a>';
         }
         if (isset($_SESSION) && ($_SESSION['id_rol'] == '1' || $_SESSION['id_rol'] == '4')) {
-            echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../usuarios/index.php">Usuarios</a>
-                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="../roles/index.php">Roles</a>'
+            echo '<a class="list-group-item list-group-item-action list-group-item-light p-3" href="../usuarios/index.php"><i class="fa-solid fa-user"></i>&nbsp;Usuarios</a>
+                <a class="list-group-item list-group-item-action list-group-item-light p-3" href="../roles/index.php"><i class="fa-solid fa-lock-open"></i>&nbsp;Roles</a>'
             ;
         }
         ?>
