8c3e4afb4cbb5b15db6c14474db850ec2cc0b6fa
Heinrich Sanchez <heinrich@H>
2023-11-11 01:18:06 -0600

    Ultimos cambios Empleados

--- model/conexion.php
+++ model/conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-    $usuario = "subasta";
-    $password = "subasta6C";
-    $conexion = new PDO("mysql:host=172.29.0.2;dbname=anf_pro_db", $usuario, $password);
+    $usuario = "root";
+    $password = "yJibcWk35Bq!";
+    $conexion = new PDO("mysql:host=localhost;dbname=anf_pro_db", $usuario, $password);
